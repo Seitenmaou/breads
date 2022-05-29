782a6761aea025d524e35851d67c08610b9eada8
Euri Tanaka <dev@mugendiary.com>
2022-05-28 23:10:55 -0700

    create and new routes complete

--- views/index.jsx
+++ views/index.jsx
@@ -18,6 +18,9 @@ function Index({breads, title}){
                     })
                 }
             </ul>
+            <div className = "newButton">
+                <a href="/breads/new"><button>Add a new Bread</button></a>
+            </div>
         </Default>
     )
 }
