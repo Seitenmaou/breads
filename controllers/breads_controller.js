49ec8ce522e32760b4b6a3116e4da596b18154c3
Euri Tanaka <dev@mugendiary.com>
2022-05-31 18:32:21 -0700

    delete route complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -11,7 +11,6 @@ breads.get('/', (req, res) => {
     //res.send(Bread)
 })
 
-module.exports = breads
 
 //NEW
 breads.get('/new', (req, res) => {
@@ -22,7 +21,8 @@ breads.get('/new', (req, res) => {
 breads.get('/:arrayIndex', (req, res) => {
     if (Bread[req.params.arrayIndex]){
         res.render('Show', {
-            bread: Bread[req.params.arrayIndex]
+            bread: Bread[req.params.arrayIndex],
+            index: req.params.arrayIndex
         })
     } else {
         res.render('404', {
@@ -36,7 +36,7 @@ breads.post('/', (req, res) => {
     console.log(req.body)
     if (!req.body.image) {
         req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
-      }
+    }
     if (req.body.hasGluten === 'on') {
         req.body.hasGluten === 'True'
     } else {
@@ -45,3 +45,11 @@ breads.post('/', (req, res) => {
     Bread.push(req.body)
     res.redirect('/breads')
 })
+
+//DELETE
+breads.delete('/:indexArray', (req, res) => {
+    Bread.splice(req.params.indexArray, 1)
+    res.status(303).redirect('/breads')
+})
+
+module.exports = breads
